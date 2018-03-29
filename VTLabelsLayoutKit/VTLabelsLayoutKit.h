500e1210960480f8843054e2b8f8365b2ae47ca5
庞翔 <pangxiangios@163.com>
2018-03-29 22:48:24 +0800

    Initial Commit

---
+++ VTLabelsLayoutKit/VTLabelsLayoutKit.h
@@ -0,0 +1,19 @@
+//
+//  VTLabelsLayoutKit.h
+//  VTLabelsLayoutKit
+//
+//  Created by pangxiang on 3/29/18.
+//  Copyright © 2018 pangxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTLabelsLayoutKit.
+FOUNDATION_EXPORT double VTLabelsLayoutKitVersionNumber;
+
+//! Project version string for VTLabelsLayoutKit.
+FOUNDATION_EXPORT const unsigned char VTLabelsLayoutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTLabelsLayoutKit/PublicHeader.h>
+
+
